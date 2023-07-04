24bc6c5cc886b978c70ec94e8c957bfbd4d2e849
luisi <luisi@yellowpencil.com>
2023-07-04 14:18:29 -0600

    new version

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -5,17 +5,21 @@
 #chown -R www-data:www-data /opt/drupal/web/sites /opt/drupal/web/modules /opt/drupal/web/themes
 #rm -Rf /var/www/html && ln -sf /opt/drupal/web /var/www/html
 
+echo "Moving files to webroot"
 cp -R /opt/drupal/* /var/www/html/
 chown -R www-data:www-data /var/www/html/
 
 # stream edit basic settings
+echo "Configuring basic settings"
 sed -i "s|DocumentRoot /var/www/html|DocumentRoot /var/www/html/web|" "/etc/apache2/sites-available/000-default.conf"
 sed -i "s|'host' => 'mysql'|'host' => 'drupal-mysql-service'|" "/etc/apache2/sites-available/000-default.conf"
 
+echo "Clearing up caches"
 cd /var/www/html && vendor/drush/drush/drush cr -y
 
-apache2-foreground
+echo "Starting apache"
+#apache2-foreground
 #service apache2 start
 #tail -f /dev/null
-#exec "$@"
+exec "$@"
 
