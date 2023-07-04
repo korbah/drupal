d9cc22ca8f9c21a92f7794618f1df4a7d0815d8f
luisi <luisi@yellowpencil.com>
2023-07-04 16:23:09 -0600

    content changes tests

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -5,19 +5,19 @@
 #chown -R www-data:www-data /opt/drupal/web/sites /opt/drupal/web/modules /opt/drupal/web/themes
 #rm -Rf /var/www/html && ln -sf /opt/drupal/web /var/www/html
 
-echo "Moving files to webroot"
+#echo "Moving files to webroot"
+#
+#cp -R /opt/drupal/* /var/www/html/
+#chown -R www-data:www-data /var/www/html/
+#
+##rm -Rf /var/www/html && ln -sf /opt/drupal/ /var/www/html
+#
+## stream edit basic settings
+#echo "Configuring basic settings"
+#sed -i "s|DocumentRoot /var/www/html|DocumentRoot /var/www/html/web|" "/etc/apache2/sites-available/000-default.conf"
+#sed -i "s|'host' => 'mysql'|'host' => 'drupal-mysql-service'|" "/etc/apache2/sites-available/000-default.conf"
 
-cp -R /opt/drupal/* /var/www/html/
-chown -R www-data:www-data /var/www/html/
-
-#rm -Rf /var/www/html && ln -sf /opt/drupal/ /var/www/html
-
-# stream edit basic settings
-echo "Configuring basic settings"
-sed -i "s|DocumentRoot /var/www/html|DocumentRoot /var/www/html/web|" "/etc/apache2/sites-available/000-default.conf"
-sed -i "s|'host' => 'mysql'|'host' => 'drupal-mysql-service'|" "/etc/apache2/sites-available/000-default.conf"
-
-#echo "Clearing up caches"
+echo "Clearing up caches"
 #cd /var/www/html && vendor/drush/drush/drush cr -y
 
 echo "Starting apache"
